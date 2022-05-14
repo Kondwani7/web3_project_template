5c24d63402b819162ae823f1034eba28f3400875
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-05-14 13:43:36 +0200

    project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h3 className="text-red-500">Web3 project</h3>
+    </div>
+  )
+}
+
+export default App
